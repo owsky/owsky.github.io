0b596d80293f06525984c423387ee703218c2701
Nicolò Bertocco <nick.bertocco@gmail.com>
2024-11-02 19:31:03 +0100

    Spellchecking

--- src/content/aboutDesc.ts
+++ src/content/aboutDesc.ts
@@ -1,4 +1,4 @@
 export const about: [string, string] = [
-  "Hey there, I'm <strong>Nicolò</strong>, though my friends call me <em>owsky</em>. I'm a software developer with a passion for backend development. I thrive on learning new concepts and experimenting with the latest technologies.",
+  "Hey there, I am <strong>Nicolò</strong>, though my friends call me <em>owsky</em>. I'm a software developer with a passion for backend development. I thrive on learning new concepts and experimenting with the latest technologies.",
   "By <em>forma mentis</em>, I appreciate strongly typed languages like Kotlin and TypeScript. My preferred development environment is VSCode on GNU/Linux.",
-]
+];
