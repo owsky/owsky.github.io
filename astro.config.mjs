555044110a7c0c6695cb3aa72b639e9abcdde344
Nicolò Bertocco <nick.bertocco@gmail.com>
2025-05-06 12:05:24 +0200

    updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import tailwindcss from '@tailwindcss/vite';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
-  , site: "https://owsky.github.io/",
-});
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [icon()],
+  site: "https://owsky.github.io/"
+})
